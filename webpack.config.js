4255f39a74b917420bdf79f7b9a76e2063bcfee8
Ivan Estruch <ivan.estruch@pivotree.com>
2023-03-14 13:08:59 +0100

    webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const {
+  glob,
+  globSync,
+  globStream,
+  globStreamSync,
+  Glob,
+} = require('glob')
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const WebpackShellPluginNext = require("webpack-shell-plugin-next");
+const mode =
+  process.env.NODE_ENV === "development" ? "development" : "production";
+const devtool = mode === "development" ? "eval-cheap-source-map" : false;
+const stats = mode === "development" ? "errors-warnings" : { children: false };
+const sass = require("node-sass");
+
+module.exports = {
+  mode: mode,
+  devtool: devtool,
+  output: {
+    path: path.resolve(__dirname, '.tmp')
+  },
+  entry: globSync('./src/**/*.js').reduce((acc, path) => {
+    const entry = (/src\/([^\/]+)\/.*/).exec(path)[1];
+    acc[entry] = `./${path}`;
+    return acc;
+  }, {}),
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './styles/[name].custom.css',
+    }),
+  ],
+  stats: stats,
+  module: {
+    rules: [
+      {
+        test: /\.(sc|sa|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              url: false,
+            },
+          },
+          "postcss-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
+if (mode === "development") {
+  module.exports.plugins.push(
+    new WebpackShellPluginNext({
+      onBuildStart: {
+        scripts: ["echo Webpack build in progress...🛠"],
+      },
+      onBuildEnd: {
+        scripts: ["echo Build Complete 📦", "theme watch", "theme open"],
+        parallel: true,
+      },
+    })
+  );
+}
