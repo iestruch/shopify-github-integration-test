0193a9753f605956d8fdc18311f552fcdbe677a7
Ivan Estruch <ivan.estruch@pivotree.com>
2023-03-30 14:12:36 +0200

    ci(pipelines): improving logs in pipeline

--- .scripts/syncdev.sh
+++ .scripts/syncdev.sh
@@ -2,13 +2,15 @@
 ORIGIN_BRANCH="main"
 DESTINY_BRANCH="develop"
 
+LAST_COMMIT_SHORT=$(git log -1 --oneline)
 LAST_COMMIT_HASH=$(git log -1 --pretty=%h)
-LAST_COMMIT_BODY=$(git log -1 --pretty=%b)
+LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%s)
 LAST_COMMIT_AUTHOR=$(git log -1 --pretty=%an)
 
+echo "Commit: $LAST_COMMIT_SHORT"
 echo $(git status -b | grep "On branch")
 echo "Processing last commit"
-echo "$LAST_COMMIT_HASH | $LAST_COMMIT_AUTHOR - $LAST_COMMIT_BODY"
+echo "$LAST_COMMIT_HASH | $LAST_COMMIT_AUTHOR - $LAST_COMMIT_MESSAGE"
 
 if [ "$LAST_COMMIT_AUTHOR" == "shopify[bot]" ];then
     git checkout develop
