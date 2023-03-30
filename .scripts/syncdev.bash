48b45ee6a2968ff7420b8b516363e1d05d2aa0eb
Ivan Estruch <ivan.estruch@pivotree.com>
2023-03-30 13:54:15 +0200

    ci(sync): sync dev from connected live
    When commit is from shopify merge branch to develop

--- .scripts/syncdev.bash
+++ .scripts/syncdev.bash
@@ -11,7 +11,8 @@ echo "$LAST_COMMIT_HASH | $LAST_COMMIT_AUTHOR - $LAST_COMMIT_BODY"
 
 if [ "$LAST_COMMIT_AUTHOR" == "shopify[bot]" ];then
     git checkout develop
-    git cherry-pick $LAST_COMMIT_HASH
+    # git cherry-pick $LAST_COMMIT_HASH
+    git merge origin/main
     git push origin develop
     echo "Shopify commit sync in $DESTINY_BRANCH correctly"
 else
