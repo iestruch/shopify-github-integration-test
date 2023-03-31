246938e4887db0b91aa1ac8be849060ffadd3a95
Ivan Estruch <ivan.estruch@pivotree.com>
2023-03-31 09:01:31 +0200

    ci(pipelines): test pipelines

--- .scripts/syncdev.sh
+++ .scripts/syncdev.sh
@@ -2,16 +2,17 @@
 ORIGIN_BRANCH="main"
 DESTINY_BRANCH="develop"
 
+LAST_COMMIT_INFO=$(git kk)
 LAST_COMMIT_HASH=$(git log -1 --pretty=%h)
 LAST_COMMIT_AUTHOR=$(git log -1 --pretty=%an)
 
-if [ ! $LAST_COMMIT_HASH ] || [ ! $LAST_COMMIT_AUTHOR ];then
+if [[ ! $LAST_COMMIT_HASH ]] || [[ ! $LAST_COMMIT_AUTHOR ]]  || [[ ! $LAST_COMMIT_INFO ]];then
     >&2 echo "ERROR: No commit info available"
     exit 1
 fi
 
 echo "Processing last commit"
-echo "${git shortlog -1 --format=reference}"
+echo "$LAST_COMMIT_INFO"
 
 if [ "$LAST_COMMIT_AUTHOR" == "shopify[bot]" ];then
     git config user.name  $LAST_COMMIT_AUTHOR
