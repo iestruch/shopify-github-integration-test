92d0352caba97d0a324279b8f5bda381e9618820
Ivan Estruch <ivan.estruch@pivotree.com>
2023-03-31 08:45:27 +0200

    ci(pipelines): ci improvements

--- .scripts/syncdev.sh
+++ .scripts/syncdev.sh
@@ -2,23 +2,18 @@
 ORIGIN_BRANCH="main"
 DESTINY_BRANCH="develop"
 
-LAST_COMMIT_SHORT=$(git log -1 --oneline)
 LAST_COMMIT_HASH=$(git log -1 --pretty=%h)
-LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%s)
 LAST_COMMIT_AUTHOR=$(git log -1 --pretty=%an)
 
-echo "Commit: $LAST_COMMIT_SHORT"
-echo $(git status -b | grep "On branch")
 echo "Processing last commit"
-echo "$LAST_COMMIT_HASH | $LAST_COMMIT_AUTHOR - $LAST_COMMIT_MESSAGE"
+echo "${git shortlog -1 --format=reference}"
 
 if [ "$LAST_COMMIT_AUTHOR" == "shopify[bot]" ];then
     git config user.name  $LAST_COMMIT_AUTHOR
-    git config user.email github-actions@github.com
-    git checkout develop
-    # git cherry-pick $LAST_COMMIT_HASH
-    git merge origin/main
-    git push origin develop
+    git config user.email no-reply@github.com
+    git checkout $DESTINY_BRANCH
+    git merge origin/$ORIGIN_BRANCH
+    git push origin $DESTINY_BRANCH
     echo "Shopify commit sync in $DESTINY_BRANCH correctly"
 else
     echo "No commit to sync"
